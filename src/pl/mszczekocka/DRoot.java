98e954b3a0db36dba659ae00411932bc8e3a9b7e
Martyna <martyna.szczekocka@gmail.com>
2021-03-27 16:12:12 +0100

    Code for:
    Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
    
    Examples
        16  -->  1 + 6 = 7
       942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
    132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
    493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2

---
+++ src/pl/mszczekocka/DRoot.java
@@ -0,0 +1,25 @@
+package pl.mszczekocka;
+
+public class DRoot {
+    public static int digital_root(int n) {
+        String number = String.valueOf(n);
+        int result = 0;
+
+        do {
+            result = 0;
+            char[] digitChar = number.toCharArray();
+            int[] digitInt = new int[digitChar.length];
+            for (int i = 0; i < digitChar.length; i++) {
+                digitInt[i] = Character.getNumericValue(digitChar[i]);
+            }
+
+            for (int i = 0; i < digitInt.length; i++) {
+                result = result + digitInt[i];
+            }
+            number = Integer.toString(result);
+
+        }while (result>=10 );
+
+        return result;
+    }
+}
