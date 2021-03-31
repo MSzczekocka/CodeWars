e35c694306c95db0e0192c9d5d78c6e7ce36c8c4
Martyna <martyna.szczekocka@gmail.com>
2021-03-31 20:49:05 +0200

    result for BitCounting

---
+++ src/pl/mszczekocka/BitCounting.java
@@ -0,0 +1,15 @@
+package pl.mszczekocka;
+
+public class BitCounting {
+    public static int countBits(int n){
+        String binaryN = Integer.toBinaryString(n);
+        int result = 0;
+
+        for (int i=0; i<binaryN.length();i++){
+            char number = binaryN.charAt(i);
+            result = result +Character.getNumericValue(number);
+        }
+
+        return result;
+    }
+}
