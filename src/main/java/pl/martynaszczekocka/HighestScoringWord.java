3a5833f24ceecb248e3819e5c808c2739423bf82
Martyna <martyna.szczekocka@gmail.com>
2021-04-24 00:55:26 +0200

    result for: HighestScoringWord

--- src/main/java/pl/martynaszczekocka/HighestScoringWord.java
+++ src/main/java/pl/martynaszczekocka/HighestScoringWord.java
@@ -3,35 +3,22 @@ package pl.martynaszczekocka;
 public class HighestScoringWord {
     public static String high(String s) {
         String[] words = s.split(" ");
-        int[] points = new int[words.length];
-        int a=0, b=0, c=0;
-        int temp =0, result=0;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int temp2 =0;
+        String result = "";
 
-        for (int i=0; i< words.length;i++){
-            char[] let = words[i].toCharArray();
-            a=0;
-            b=0;
-            c=0;
-            for (int j=0; j< let.length; j++){
-                if(let[j]=='a'){
-                    a = a + 1;
-                }else if (let[j]=='b'){
-                    b=b+2;
-                }else if (let[j]=='c'){
-                    c=c+3;
-                }
+        for (int i=0; i< words.length; i++){
+            int temp=0;
+            char[] letters = words[i].toCharArray();
+            for (char letter: letters){
+                temp = temp + alphabet.indexOf(letter)+1;
             }
-            points[i] = a+b+c;
+            if (temp>temp2){
+                temp2 = temp;
+                result = words[i];
+            }
         }
 
-        for (int i=0; i< words.length;i++){
-            if (points[i]>temp){
-                temp = points[i];
-                result=i;
-            }
-
-        }
-
-        return words[result];
+        return result;
     }
 }
