1ca9456b2260a85e5d5c3b80fce6898e2bb388f4
Martyna <martyna.szczekocka@gmail.com>
2021-06-17 18:06:00 +0200

    BiggerNumber - in progress

--- src/main/java/pl/martynaszczekocka/BiggerNumber.java
+++ src/main/java/pl/martynaszczekocka/BiggerNumber.java
@@ -4,20 +4,30 @@ public class BiggerNumber {
 
     public static long nextBiggerNumber(long n){
         String number = Long.toString(n);
-        long result = -1;
+        long tempNum = n*10;
+        long newNumL = 0L;
+        String newNumS = "";
 
         for (int i=0; i< number.length();i++){
             char digit = number.charAt(i);
+            String tempN = number.substring(0,i) +number.substring(i+1);
             for(int j=0; j<number.length(); j++){
-                String newNumS = number.substring(0,i) + digit + number.substring(i+1);
-                long newNumL = Long.getLong(newNumS);
+                newNumS = tempN.substring(0,j) + digit + tempN.substring(j+1);
+                newNumL = Long.parseLong(newNumS);
 
+                if (newNumL==n+1){
+                    return n+1;
+                } else if (newNumL>n && tempNum>newNumL){
+                    tempNum = newNumL;
+                }
 
             }
         }
 
+        if (tempNum == n*10)
+            return -1;
 
-        return n;
+        return tempNum;
     }
 
 
